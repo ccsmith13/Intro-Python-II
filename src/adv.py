571986d110f0437e88a852e213e75897aed6da75
Christine Smith <christinecsmith1@gmail.com>
2020-09-08 19:32:32 -0700

    starting the take items part of the assignment

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,32 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
-
+all_items = {
+    'candlestick': Item('Candlestick', 'A tarnished silver candlestick. Heavy.'),
+    'match': Item('Match', 'Matches can start fires.'),
+    'boulder': Item('Boulder', 'A rock as tall and wide as you are tall.'),
+    'cobweb': Item('Cobweb', 'Yuck, does that mean there are spiders?'),
+    'dubloons': Item('Dubloons', 'A large canvas sack containing one million dubloons - jackpot!')
+}
 room = {
     'outside':  Room("Outside Cave Entrance",
                      "North of you, the cave mount beckons"),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
-passages run north and east."""),
+passages run north and east.""", [all_items['candlestick'], all_items['match']]),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""", [all_items['boulder']]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
-to north. The smell of gold permeates the air."""),
+to north. The smell of gold permeates the air.""", [all_items['cobweb']]),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""", [all_items['dubloons']]),
 }
 
 
@@ -54,16 +61,26 @@ next_direction = 'placeholder'
 while next_direction: 
     print('You are currently at the', new_player.location.name)
     print(new_player.location.description)
-    print('Which direction would you like to go to? Enter one of the following: (n, s, e, w')
-    next_direction = input()
-    if next_direction == 'q':
-        next_direction = ''
-        print('Exiting game. Thank you for playing!')
+    print('Items in this room:')
+    if new_player.location.item_list == []:
+        print('No items available')
     else:
-        next_direction = next_direction + '_to'
-        try: 
-            x = getattr(new_player.location, next_direction)
-            print('You can move this direction.')
-            new_player.location = x
-        except AttributeError:
-            print('A pile of bricks blocks your path. Try a different direction?')
+        for item in new_player.location.item_list:
+            print(item.name, item.description) 
+    print('Which direction would you like to go to? Or do something else? Enter one of the following: (n, s, e, w, or take/get itemname)')
+    next_direction = input()
+    if lower(next_direction)[0:4] == 'take' or 'get ':
+        print('The player wants to take an item')
+        next_direction = ''
+    else:
+        if next_direction == 'q':
+            next_direction = ''
+            print('Exiting game. Thank you for playing!')
+        else:
+            next_direction = next_direction + '_to'
+            try: 
+                x = getattr(new_player.location, next_direction)
+                print('You can move this direction.')
+                new_player.location = x
+            except AttributeError:
+                print('A pile of bricks blocks your path. Try a different direction?')
--- src/room.py
+++ src/room.py
@@ -1,6 +1,7 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 class Room():
-    def __init__(self, name, description):
+    def __init__(self, name, description, item_list=[]):
         self.name = name
         self.description = description
+        self.item_list = item_list
